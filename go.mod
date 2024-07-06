37a8a3526f890df3f1c35c186ba6e0ca4a039526
Steven Hartland <stevenmhartland@gmail.com>
2024-07-06 17:01:53 +0100

    perf: inline binary search
    Use inline version of binary search instead of sort.Search which is
    ~50% quicker in most cases.
    
    benchstat -filter '.unit:ns/op' orig.log inline.log
    goos: linux
    goarch: amd64
    pkg: github.com/hashicorp/go-immutable-radix/v2
    cpu: Intel(R) Core(TM) i9-14900HX
                                                  │   orig.log    │             inline.log             │
                                                  │    sec/op     │   sec/op     vs base               │
    LongestPrefix/words-exact-match-early-32         59.47n ± 12%   29.70n ± 1%  -50.06% (p=0.002 n=6)
    LongestPrefix/words-exact-match-late-32          56.99n ±  2%   33.43n ± 3%  -41.35% (p=0.002 n=6)
    LongestPrefix/words-prefix-match-early-32       106.85n ±  3%   52.09n ± 2%  -51.24% (p=0.002 n=6)
    LongestPrefix/words-prefix-match-late-32         75.12n ±  2%   38.78n ± 2%  -48.37% (p=0.002 n=6)
    LongestPrefix/words-not-found-early-32          12.945n ±  2%   5.267n ± 3%  -59.31% (p=0.002 n=6)
    LongestPrefix/words-not-found-late-32           12.415n ±  2%   4.577n ± 4%  -63.13% (p=0.002 n=6)
    LongestPrefix/uuids-exact-match-early-32         61.14n ±  4%   33.76n ± 7%  -44.78% (p=0.002 n=6)
    LongestPrefix/uuids-exact-match-late-32          60.79n ±  4%   35.28n ± 6%  -41.96% (p=0.002 n=6)
    LongestPrefix/uuids-prefix-match-early-32        57.02n ±  8%   32.57n ± 4%  -42.87% (p=0.002 n=6)
    LongestPrefix/uuids-prefix-match-late-32         58.01n ±  2%   34.44n ± 3%  -40.63% (p=0.002 n=6)
    LongestPrefix/uuids-not-found-early-32           48.36n ±  3%   30.74n ± 3%  -36.44% (p=0.002 n=6)
    LongestPrefix/uuids-not-found-late-32           11.600n ±  4%   4.099n ± 6%  -64.66% (p=0.002 n=6)
    LongestPrefix/endpoints-exact-match-early-32     57.82n ±  4%   34.01n ± 3%  -41.18% (p=0.002 n=6)
    LongestPrefix/endpoints-exact-match-late-32      73.75n ±  4%   41.46n ± 3%  -43.77% (p=0.002 n=6)
    LongestPrefix/endpoints-prefix-match-early-32    51.55n ±  4%   30.58n ± 1%  -40.69% (p=0.002 n=6)
    LongestPrefix/endpoints-prefix-match-late-32     63.00n ±  4%   40.37n ± 4%  -35.92% (p=0.002 n=6)
    LongestPrefix/endpoints-not-found-early-32       6.098n ±  4%   2.606n ± 6%  -57.26% (p=0.002 n=6)
    LongestPrefix/endpoints-not-found-late-32        6.188n ±  3%   2.646n ± 3%  -57.23% (p=0.002 n=6)
    geomean                                          36.86n         18.93n       -48.63%

--- go.mod
+++ go.mod
@@ -5,5 +5,12 @@ go 1.18
 require (
 	github.com/hashicorp/go-uuid v1.0.3
 	github.com/hashicorp/golang-lru/v2 v2.0.0
+	github.com/stretchr/testify v1.9.0
 	golang.org/x/exp v0.0.0-20221215174704-0915cd710c24
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- node_test.go
+++ node_test.go
@@ -1,7 +1,12 @@
 package iradix
 
 import (
+	"bufio"
+	"os"
+	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestNodeWalk(t *testing.T) {
@@ -52,4 +57,111 @@ func TestNodeWalkBackwards(t *testing.T) {
 
 		return false
 	})
+}
+
+// loadLines loads the lines from a file in the testdata directory.
+func loadLines(tb testing.TB, filename string) []string {
+	tb.Helper()
+
+	f, err := os.Open(filepath.Join("testdata", filename))
+	require.NoError(tb, err)
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	require.NoError(tb, scanner.Err())
+
+	return lines
+}
+
+type benchTest struct {
+	name     string
+	prefix   string
+	expected string
+}
+
+var longestPrefixTests = []struct {
+	filename string
+	tests    []benchTest
+}{
+	{
+		filename: "words",
+		tests: []benchTest{
+			{name: "exact-match-early", prefix: "ageless", expected: "ageless"},
+			{name: "exact-match-late", prefix: "Wachenheimer", expected: "Wachenheimer"},
+			{name: "prefix-match-early", prefix: "archconspirator+other", expected: "archconspirator"},
+			{name: "prefix-match-late", prefix: "ureametry+other", expected: "ureametry"},
+			{name: "not-found-early", prefix: "0000-notfound"},
+			{name: "not-found-late", prefix: "~xxx-notfound"},
+		},
+	},
+	{
+		filename: "uuids",
+		tests: []benchTest{
+			{name: "exact-match-early", prefix: "85f9f198-6bae-4c57-a61d-ee31be8dcec3", expected: "85f9f198-6bae-4c57-a61d-ee31be8dcec3"},
+			{name: "exact-match-late", prefix: "b938fb33-9ed7-4658-9b88-faa82443645c", expected: "b938fb33-9ed7-4658-9b88-faa82443645c"},
+			{name: "prefix-match-early", prefix: "fcb5e951-feed-4a54-98fb-281b6a3f175c+other", expected: "fcb5e951-feed-4a54-98fb-281b6a3f175c"},
+			{name: "prefix-match-late", prefix: "a17b956c-438c-4932-aff8-7e66093022d8+something", expected: "a17b956c-438c-4932-aff8-7e66093022d8"},
+			{name: "not-found-early", prefix: "0000-notfound"},
+			{name: "not-found-late", prefix: "~xxx-notfound"},
+		},
+	},
+	{
+		filename: "endpoints",
+		tests: []benchTest{
+			{name: "exact-match-early", prefix: "/applications/{client_id}/token/scoped", expected: "/applications/{client_id}/token/scoped"},
+			{name: "exact-match-late", prefix: "/users/{username}/packages/{package_type}/{package_name}", expected: "/users/{username}/packages/{package_type}/{package_name}"},
+			{name: "prefix-match-early", prefix: "/gists/{gist_id}+other", expected: "/gists/{gist_id}"},
+			{name: "prefix-match-late", prefix: "/user/installations+other", expected: "/user/installations"},
+			{name: "not-found-early", prefix: "0000-notfound"},
+			{name: "not-found-late", prefix: "~xxx-notfound"},
+		},
+	},
+}
+
+func TestLongestPrefixLong(t *testing.T) {
+	for _, tc := range longestPrefixTests {
+		for _, test := range tc.tests {
+			t.Run(tc.filename+"-"+test.name, func(t *testing.T) {
+				lines := loadLines(t, tc.filename+".txt")
+				r := New[struct{}]()
+				txn := r.Txn()
+				for _, l := range lines {
+					txn.Insert([]byte(l), struct{}{})
+				}
+
+				root := txn.Commit().Root()
+
+				got, _, _ := root.LongestPrefix([]byte(test.prefix))
+				require.Equal(t, test.expected, string(got))
+			})
+		}
+	}
+}
+
+func BenchmarkLongestPrefix(b *testing.B) {
+	for _, tc := range longestPrefixTests {
+		for _, test := range tc.tests {
+			b.Run(tc.filename+"-"+test.name, func(b *testing.B) {
+				lines := loadLines(b, tc.filename+".txt")
+				r := New[struct{}]()
+				txn := r.Txn()
+				for _, l := range lines {
+					txn.Insert([]byte(l), struct{}{})
+				}
+
+				root := txn.Commit().Root()
+
+				b.ResetTimer()
+				for i := 0; i < b.N; i++ {
+					root.LongestPrefix([]byte(test.prefix))
+				}
+			})
+		}
+	}
 }
